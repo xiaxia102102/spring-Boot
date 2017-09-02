b47ca4af0274903f81278b68d3df44a8e211acc6
xiaxia102102 <xiaxia102102@163.com>
2017-09-02 21:08:46 +0800

    Initial commit

---
+++ src/main/java/SpringController.java
@@ -0,0 +1,22 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class SpringController {
+    //设置访问的url
+    @RequestMapping("/")
+    //表示返回JSON格式的结果，如果前面使用的是@RestController可以不用写
+    @ResponseBody
+    String home() {
+        return "Hello World!";//返回结果为字符串
+    }
+
+    public static void main(String[] args) throws Exception {
+        //通过SpringApplication的run()方法启动应用，无需额外的配置其他的文件
+        SpringApplication.run(SpringController.class, args);
+    }
+}
---
+++ src/test/java/SpringController.java
@@ -0,0 +1,20 @@
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+@Controller
+@EnableAutoConfiguration
+public class SpringController {
+    //设置访问的url
+    @RequestMapping("/")
+    //表示返回JSON格式的结果，如果前面使用的是@RestController可以不用写
+    @ResponseBody
+    String home() {
+        return "Hello World!";//返回结果为字符串
+    }
+
+    public static void main(String[] args) throws Exception {
+        //通过SpringApplication的run()方法启动应用，无需额外的配置其他的文件
+        SpringApplication.run(SpringController.class, args);
+    }
+}
